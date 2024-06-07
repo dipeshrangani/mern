4ea492f64518fb246e1b0b9081c2a5cbd44ca423
Dipesh <dipeshrangani23@gmail.com>
2024-06-07 11:45:29 +0530

    Add express server and connect to MongoDB

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
+                <h1 className='font-bold'>
+                    <span className='text-slate-500'>Royale</span>
+                    <span className='text-slate-700'>Estate</span>
+                </h1>
+            </Link>
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+                <FaSearch className="text-slate-600" />
+            </form>
+            <ul className='flex gap-4'>
+                <li>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/sign-in'>
+                        Sign In
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </header>
+  )
+}
